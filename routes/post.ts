994d10cd24728e44d7d3a280da679d46bcc6a75b
h662 <h662hong@gmail.com>
2023-10-13 16:32:20 +0900

    GET post/id, post/userId

--- routes/post.ts
+++ routes/post.ts
@@ -56,12 +56,12 @@ router.get("/", async (req, res) => {
     if (!page) {
       return res.status(400).json({
         ok: false,
-        message: "Need page.",
+        message: "Not exist page.",
       });
     }
 
     const posts = await client.post.findMany({
-      skip: +page! * 3,
+      skip: +page * 3,
       take: 3,
       select: {
         id: true,
@@ -81,7 +81,103 @@ router.get("/", async (req, res) => {
     if (posts.length === 0) {
       return res.status(400).json({
         ok: false,
-        message: "Not exist contents.",
+        message: "Not exist posts.",
+      });
+    }
+
+    return res.json({ ok: true, posts });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      ok: false,
+      message: "Server error.",
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({
+        ok: false,
+        message: "Not exist data.",
+      });
+    }
+
+    const post = await client.post.findFirst({
+      where: {
+        id: +id,
+      },
+      select: {
+        id: true,
+        content: true,
+        userId: true,
+        user: {
+          select: {
+            account: true,
+          },
+        },
+      },
+    });
+
+    if (!post) {
+      return res.status(400).json({
+        ok: false,
+        message: "Not exist post.",
+      });
+    }
+
+    return res.json({ ok: true, post });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      ok: false,
+      message: "Server error.",
+    });
+  }
+});
+
+router.get("/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { page } = req.query;
+
+    if (!userId || !page) {
+      return res.status(400).json({
+        ok: false,
+        message: "Not exist data.",
+      });
+    }
+
+    const posts = await client.post.findMany({
+      where: {
+        userId: +userId,
+      },
+      skip: +page * 3,
+      take: 3,
+      select: {
+        id: true,
+        content: true,
+        userId: true,
+        user: {
+          select: {
+            account: true,
+          },
+        },
+      },
+      orderBy: {
+        id: "desc",
+      },
+    });
+
+    if (posts.length === 0) {
+      return res.status(400).json({
+        ok: false,
+        message: "Not exist posts.",
       });
     }
 
