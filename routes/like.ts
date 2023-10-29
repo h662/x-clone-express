0f135af6a6b0b8dd1475e3146f04312d1ca92ff7
h662 <h662hong@gmail.com>
2023-10-29 16:23:43 +0900

    Like

--- routes/like.ts
+++ routes/like.ts
@@ -1,4 +1,4 @@
-import express, { Request } from "express";
+import express from "express";
 import { PrismaClient } from "@prisma/client";
 import { verifyToken } from "./auth";
 
@@ -6,12 +6,146 @@ const router = express.Router();
 
 const client = new PrismaClient();
 
-router.put("/", async (req, res) => {
+router.put("/", verifyToken, async (req, res) => {
   try {
-    const { postId, commentId } = req.body;
+    const { postId } = req.query;
     const { account } = req;
 
-    console.log(account);
+    if (!postId) {
+      return res.status(400).json({
+        ok: false,
+        message: "Not exist data.",
+      });
+    }
+
+    const user = await client.user.findUnique({
+      where: {
+        account,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        message: "Not exist user.",
+      });
+    }
+
+    const post = await client.post.findUnique({
+      where: {
+        id: +postId,
+      },
+    });
+
+    if (!post) {
+      return res.status(400).json({
+        ok: false,
+        message: "Not exist post.",
+      });
+    }
+
+    let like = await client.like.findFirst({
+      where: {
+        postId: +postId,
+      },
+    });
+
+    if (!like) {
+      like = await client.like.create({
+        data: {
+          postId: +postId,
+        },
+      });
+    }
+
+    await client.like.update({
+      where: {
+        id: like.id,
+      },
+      data: {
+        isLiked: true,
+      },
+    });
+
+    return res.json({
+      ok: true,
+      like,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      ok: false,
+      message: "Server error.",
+    });
+  }
+});
+
+router.put("/", verifyToken, async (req, res) => {
+  try {
+    const { commentId } = req.query;
+    const { account } = req;
+
+    if (!commentId) {
+      return res.status(400).json({
+        ok: false,
+        message: "Not exist data.",
+      });
+    }
+
+    const user = await client.user.findUnique({
+      where: {
+        account,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        message: "Not exist user.",
+      });
+    }
+
+    const comment = await client.comment.findUnique({
+      where: {
+        id: +commentId,
+      },
+    });
+
+    if (!comment) {
+      return res.status(400).json({
+        ok: false,
+        message: "Not exist comment.",
+      });
+    }
+
+    let like = await client.like.findFirst({
+      where: {
+        commentId: +commentId,
+      },
+    });
+
+    if (!like) {
+      like = await client.like.create({
+        data: {
+          commentId: +commentId,
+        },
+      });
+    }
+
+    await client.like.update({
+      where: {
+        id: like.id,
+      },
+      data: {
+        isLiked: true,
+      },
+    });
+
+    return res.json({
+      ok: true,
+      like,
+    });
   } catch (error) {
     console.error(error);
 
